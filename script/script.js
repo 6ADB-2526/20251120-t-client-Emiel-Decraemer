c6c5c016a16dc52afb72a16906d5f287b402bfbd
emieldecraemer <emiel.decraemer@student.tsaam.be>
2025-11-21 11:50:48 +0100

    kinderenInLijst

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 /**
  * 
  */
+
+const selectKinderen = document.querySelector("#kinderen")
+
+
+fetch("https://o-apiandclient-render.onrender.com/kinderen")
+.then(info => info.json())
+.then(kinderen => {
+    kinderen.forEach(kind => {
+        const newoption = document.createElement("option")
+        newoption.innerHTML = kind.voornaam
+        selectKinderen.appendChild(newoption)
+    });
+})
